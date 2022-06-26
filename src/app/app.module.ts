f971a2fba38f6406c669de70112517c1f82accdf
José Mumo <mjose@live.com.ar>
2022-06-26 18:56:43 -0300

    Primera versión

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarraSuperiorComponent } from './componentes/barra-superior/barra-superior.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExpComponent } from './componentes/exp/exp.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { PieComponent } from './componentes/pie/pie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarraSuperiorComponent
+    BarraSuperiorComponent,
+    PerfilComponent,
+    AcercaDeComponent,
+    ExpComponent,
+    EducacionComponent,
+    AptitudesComponent,
+    ProyectosComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule,
